3d42777d29e75f9932f560df9232c5524453628f
Pavan Saragadam <pavan.saragadam@jobiak.ai>
2020-09-02 20:46:47 +0530

    init changes

---
+++ src/main/java/com/store/storeBilling/StoreBillingApplication.java
@@ -0,0 +1,13 @@
+package com.store.storeBilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/storeBilling/StoreBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.storeBilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
