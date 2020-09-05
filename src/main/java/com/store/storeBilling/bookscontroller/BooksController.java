05b6da85c6dcea7f74e19d04ce5f8687a57e0449
soujanyagude <soujanyagude@soujanyas-air.lan>
2020-09-04 22:08:27 -0400

    Book store services

---
+++ src/main/java/com/store/storeBilling/bookscontroller/BooksController.java
@@ -0,0 +1,31 @@
+package com.store.storeBilling.bookscontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.store.storeBilling.booksmodel.Books;
+import com.store.storeBilling.booksrepo.BooksRepo;
+
+//@SuppressWarnings("unused")
+@RestController
+@RequestMapping(path= "/Books")
+public class BooksController {
+	
+	@Autowired
+	
+	private BooksRepo booksrepo;
+	
+	@GetMapping(path= "/AddBooks")
+	public void AddBooksList() {
+		
+		List<Books> bookList = booksrepo.creatRepository();
+	}
+	
+	
+
+}
