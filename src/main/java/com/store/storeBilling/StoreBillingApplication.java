5e5a6adccffbd36ae63a7b105241e3da513ca7e2
Pavan Saragadam <pavan.saragadam@jobiak.ai>
2020-09-08 07:16:11 +0530

    change at application

--- src/main/java/com/store/storeBilling/StoreBillingApplication.java
+++ src/main/java/com/store/storeBilling/StoreBillingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreBillingApplication {
 
 	/**
-	 * test
+	 * main application
 	 * @param args
 	 */
 	public static void main(String[] args) {
